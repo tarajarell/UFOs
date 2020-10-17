bd689c84006b46ddf23fd08fae027876e4165723
tarajarellgit <ms.taraj@gmail.com>
2020-10-17 10:01:14 -0700

    del 2 images; fixed code for filters

--- static/js/app.js
+++ static/js/app.js
@@ -58,7 +58,7 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    Object.defineProperties(filters).forEach(([key, value]) => {
+    Object.entries(filters).forEach(([key, value]) => {
       filteredData = filteredData.filter(row => row[key] === value);
     });
   
